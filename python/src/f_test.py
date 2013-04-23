ca88b0502607c658e24de0dc733cee6720a8eb50
zhangyong <zhangyong_student@163.com>
2013-04-23 15:10:42 +0800

    Update f_test.py

--- python/src/f_test.py
+++ python/src/f_test.py
@@ -4,3 +4,7 @@ x=theano.tensor.vector('x')
 y=theano.tensor.vector('y')
 f=theano.function(inputs=[x,y],outputs=x+y*2)
 print f([1,2],[3,4])
+
+
+#zhe shi wo gai de 
+print" ni he bing xia wo kankan "
